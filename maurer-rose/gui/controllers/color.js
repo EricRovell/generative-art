0e009352ad1417a3974bc4671b05eb7a8389c48f
Eric Rovell <ericrovell@gmail.com>
2019-11-01 23:07:27 +0300

    color input: now it is possible to use HEX and RGBA in one place

--- maurer-rose/gui/controllers/color.js
+++ maurer-rose/gui/controllers/color.js
@@ -2,7 +2,7 @@ import Container from "./container.js";
 import Label from "./label.js";
 import Value from "./value.js";
 
-import { rgb2hex } from "../utility/convertColor.js";
+import { rgba2hexa, hexa2rgba, rgb2hex, hex2rgb } from "../utility/convertColor.js";
 
 const evalRGB = string => {
   const regex = /((25[0-5]|2[0-4]\d|1\d{1,2}|\d\d?)\s*,\s*?){2}(25[0-5]|2[0-4]\d|1\d{1,2}|\d\d?)\s*,?\s*([01]\.?\d*?)?/
@@ -12,32 +12,63 @@ const evalRGB = string => {
       .map(Number);
     return rgb;
   }
-}
+};
+
+const testHEX = value => {
+  const regex = /[#](([0-9A-F]{8})|([0-9A-F]{6})|([0-9A-F]{4})|([0-9A-F]{3}))/i;
+  return regex.test(value);
+};
+
+const testRGBA = value => {
+  const regex = /((25[0-5]|2[0-4]\d|1\d{1,2}|\d\d?)\s*,\s*?){2}(25[0-5]|2[0-4]\d|1\d{1,2}|\d\d?)\s*,?\s*([01]\.?\d*?)?/;
+  return regex.test(value);
+};
 
 export default ({ name, initial, variable }, state) => {
   const container = Container();
 
   const label = Label({ name });
-  const colorField = Value({ initial, type: "text" });
+  const colorInput = Value({ initial, type: "text" });
 
   const rgb = initial
       .split(",")
       .map(Number);
   const hex = rgb2hex(...rgb.slice(0, 3));
-  const colorLabel = Value({ initial: hex, type: "color" });
 
-  colorField.addEventListener("input", event => {
-    const colorInput = event.target.value;
-    const rgb = evalRGB(colorInput);
-    if (rgb) {
-      state[variable] = `rgba(${rgb})`;      
-      const hex = rgb2hex(...rgb.slice(0, 3));
+  const colorLabel = Value({ initial: hex, type: "color" });
+  colorLabel.addEventListener("input", event => {
+    const picked = event.target.value.toUpperCase();
+    // change form to HEX-value only if user USED HEX-color before change
+    if (testHEX(colorInput.value)) {
+      colorInput.value = picked;
+      state[variable] = picked;
+    }
+    if (testRGBA(colorInput.value)) {
+      const rgb = hexa2rgba(picked);
+      colorInput.value = rgb;
+      state[variable] = rgb;
+    }
+  });
+
+  colorInput.addEventListener("input", event => {
+    const color = event.target.value;
+    if (testRGBA(color)) {
+      const rgb = color.split(",").map(Number);
+      const hex = rgb2hex(...rgb);
+      console.log(`rgba(${rgb.join(",")})`);
       colorLabel.value = hex;
-    }      
+      state[variable] = `rgba(${rgb.join(",")})`;
+    }
+    if (testHEX(color)) {
+      if (color.length >= 7) {
+        colorLabel.value = color.slice(0, 7);
+      }
+      state[variable] = `${color}`;
+    }    
   });
 
 
   container.classList.add("gui-color");
-  container.append(label, colorLabel, colorField);
+  container.append(label, colorLabel, colorInput);
   return container;
 };
