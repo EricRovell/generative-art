ca5d06b15146bec9096343b8945fe03f60ba1eaf
Eric Rovell <ericrovell@gmail.com>
2019-11-01 23:06:58 +0300

    new convert function for colours

--- maurer-rose/gui/utility/convertColor.js
+++ maurer-rose/gui/utility/convertColor.js
@@ -4,15 +4,31 @@ const toRange = (value, a, b, c, d) => {
   return c + Math.round(slope * (value - a));
 };
 
-export const rgba2hexa = (r, g, b, a) => {
+export const rgba2hexa = (r, g, b, a = 0) => {
   // convert channels
   const rgb = [ r, g, b ].map(channel => {
     const hex = channel.toString(16);
     return hex.length === 1 ? `0${hex}` : hex;
   });
+  // if a is still 0, it is rgb, return the result
+  if (a === 0) return [ ...rgb ]; 
   // map rgba alpha channel to hex range
   const alpha = toRange(a, 0, 1, 0, 255).toString(16);
   return [ "#", ...rgb, alpha ].join("");
+};
+
+export const hexa2rgba = hexa => {
+  const regex = /^(#{0,1})([0-9A-F]{2})([0-9A-F]{2})([0-9A-F]{2})([0-9A-F]{2})?$/i;
+
+  const channels = [ ...hexa.match(regex).slice(2).filter(val => val !== undefined) ];
+  const rgb = [ ...channels ].map(channel => parseInt(channel, 16));
+
+  if (channels.length !== 3) {
+    const alpha = Number((rgb[3] / 255).toFixed(2));
+    return [ ...rgb.slice(0, 3), alpha];
+  }
+
+  return rgb;
 };
 
 export const rgb2hex = (r, g, b) => "#" + (
